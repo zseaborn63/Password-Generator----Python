caf9749e81360545cc7c70982a22ed1b593e2721
Zach Seaborn <zach@seaborn.dev>
2026-03-19 18:40:35 -0400

    Initial Runnable Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def generate_password():
+    print("Hello Password Generator!")
+
+    # TODO:  Get user input for security level 1-3
+
+    # TODO:  Fetch words from API
+
+    # TODO:  Replace one letter w/ integer
+
+    # TODO:  Replace one letter w/ special character
+
+    # TODO:  Misspell one word in order to introduce security
+
+if __name__ == "__main__":
+    generate_password()
+    exit(0)
